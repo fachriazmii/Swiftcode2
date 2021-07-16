c31fd6ce8d80d638e691da527520706df1d5de5f
fachriazmi <fachriazmi9@gmail.com>
2021-07-16 11:36:11 +0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Muhammad Fahri Azmi on 16/07/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
